36a5dc0a1a52657abc07127088169a73c43d54e1
Dima Tisnek <dimaqq@gmail.com>
2021-09-03 18:05:55 +0900

    add parallel test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,21 @@
 import os
+import secrets
 
+import aiohttp
 import pytest
+
+
+@pytest.fixture
+def random_name():
+    return secrets.token_hex(nbytes=4)
 
 
 @pytest.fixture
 def server():
     return os.environ.get("EBI_HOST", "localhost")
+
+
+@pytest.fixture
+async def client():
+    async with aiohttp.ClientSession() as client:
+        yield client
--- tests/test_ebi.py
+++ tests/test_ebi.py
@@ -1,3 +1,4 @@
+import asyncio
 import secrets
 import time
 from contextlib import contextmanager
@@ -24,12 +25,22 @@ def canonical_time(time):
             return threshold
 
 
-async def test_sequential_requests(server):
-    tag = secrets.token_hex(4)
+async def test_sequential_requests(client, server, random_name):
     log = []
-    async with aiohttp.ClientSession() as client:
-        for _ in range(4):
-            with timed(save_to=log):
-                resp = await client.get(f"http://{ server }:7890/backoff/{ tag }")
-                assert resp.status in (200, 204)
+    for _ in range(4):
+        with timed(save_to=log):
+            resp = await client.get(f"http://{ server }:7890/backoff/{ random_name }")
+            assert resp.status in (200, 204)
+
     assert [canonical_time(t) for t in log] == [0.1, 0.2, 0.4, 0.8]
+
+
+async def test_parallel_requests(client, server, random_name):
+    async def request():
+        with timed(save_to=log):
+            resp = await client.get(f"http://{ server }:7890/backoff/{ random_name }")
+            assert resp.status in (200, 204)
+
+    log = []
+    await asyncio.gather(*[request() for i in range(4)])
+    assert [canonical_time(t) for t in sorted(log)] == [0.1, 0.2, 0.4, 0.8]
